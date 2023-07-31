a29822ccc35adab1b1c1f4801a60adcfdb2d954f
Nipan Debnath <debnathnipan719@gmail.com>
2023-07-31 23:08:08 +0530

    use latest Router version

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,43 @@ import Header from "./components/Header";
 import Home from "./pages/Home.jsx";
 import Cart from "./pages/Cart.jsx";
 import Account from "./pages/Account";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider, Outlet, createBrowserRouter } from "react-router-dom";
 import ProductView from "./pages/ProductView";
+import ProductsList from "./components/ProductsList";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+	return (
+		<main className="container mx-auto px-6">
+			<Header />
+			<Outlet />
+			<Footer />
+		</main>
+	);
+};
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/products/:id",
+				element: <ProductsList />,
+			},
+			{
+				path: "/product/:id",
+				element: <ProductView />,
+			},
+		],
+	},
+]);
 
 function App() {
-	return (
-		<BrowserRouter>
-			<main className="container mx-auto px-6">
-				<Header />
-				<Routes>
-					<Route path="" element={<Home />} />
-					<Route path="/products">
-						<Route path=":id" element={<ProductView />}></Route>
-					</Route>
-
-					<Route path="/cart" element={<Cart />} />
-
-					<Route path="/account" element={<Account />} />
-				</Routes>
-			</main>
-		</BrowserRouter>
-	);
+	return <RouterProvider router={router}></RouterProvider>;
 }
 export default App;
