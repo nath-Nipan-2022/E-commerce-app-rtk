e32c1118fc9824113a6c408739daf84ab592ea69
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-02 22:36:16 +0530

    style search-field

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,13 +2,13 @@ import { GoSearch } from "react-icons/go";
 
 function SearchForm({ searchQuery, className }) {
 	return (
-		<div className={`hidden md:flex items-center rounded-full bg-gray-100`}>
+		<div className={`p-2 hidden md:flex items-center rounded-full bg-gray-100`}>
 			<input
-				className="p-2 pl-4 flex-1 bg-transparent outline-none"
+				className="pl-2 flex-1 bg-transparent outline-none placeholder:text-gray-500"
 				type="search"
 				placeholder="Search Product"
 			/>
-			<span className="p-3">{<GoSearch />}</span>
+			<span className="p-1">{<GoSearch className="text-gray-600" />}</span>
 		</div>
 	);
 }
