d2eca77569e9326ac0710060d98b92ae6bece133
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-02 23:59:21 +0530

    update header  for Navbar.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,20 @@
-import { GoArrowDown, GoChevronDown, GoPerson } from "react-icons/go";
-import { BsMenuApp } from "react-icons/bs";
-import { BsCart2 } from "react-icons/bs";
+import { GoPerson } from "react-icons/go";
+import { BsMenuApp, BsCart2 } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import SearchForm from "./SearchForm";
 import logo from "../assets/shopping logo.png";
-import { useRef, useState } from "react";
-import Submenus from "./SubmenuLink";
+import { useState } from "react";
+import Navbar from "./Navbar";
 
 function Header() {
-	const navLinks = [
-		{
-			id: 1,
-			label: "Categories",
-			path: "/",
-			icon: <GoChevronDown />,
-			children: ["Men", "Women", "Kids"],
-		},
-		{
-			id: 2,
-			label: "Deals",
-			path: "/deals",
-		},
-		{
-			id: 3,
-			label: "What's New",
-			path: "/whats-new",
-		},
-		{
-			id: 4,
-			label: "Delivery",
-			path: "/delivery",
-		},
-	];
+	const [openMenu, setOpenMenu] = useState(false);
 
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const onMenuClose = () => {
+		setOpenMenu((prev) => !prev);
+	};
 
 	return (
-		<header className="flex justify-between items-center">
+		<header className="flex justify-between items-center p-2">
 			<div>
 				<Link to={"/"} className="flex items-center font-medium">
 					<img src={logo} alt="company logo" width={56} />
@@ -44,45 +22,7 @@ function Header() {
 				</Link>
 			</div>
 
-			<nav
-				className={`fixed z-20 w-full h-screen left-0 top-0 pt-[76px] px-10 lg:p-0 lg:relative lg:left-auto lg:top-auto lg:mx-0 lg:h-[56px] lg:w-auto transition-colors duration-500 ${
-					isMenuOpen ? "bg-black/30 visible" : "invisible"
-				} lg:visible lg:flex lg:items-center`}
-				onClick={() => setIsMenuOpen(false)}
-			>
-				<ul
-					className={`mx-auto max-w-2xl lg:max-w-7xl flex flex-col lg:flex-row lg:items-center  md:gap-4 rounded-lg p-3 bg-gray-100 lg:p-0 lg:rounded-none lg:bg-transparent transition duration-500 ${
-						isMenuOpen
-							? "translate-y-0 opacity-100"
-							: "-translate-y-4 opacity-0"
-					} 
-					lg:opacity-100 lg:translate-y-0 `}
-				>
-					{navLinks.map((link) => {
-						return (
-							<li key={link.id} className="font-medium relative group">
-								<Link
-									to={link.path}
-									className="flex items-center gap-2 p-1 px-1 rounded hover:bg-blue-500 hover:text-white lg:hover:bg-transparent lg:hover:text-slate-800"
-								>
-									<span>{link.label}</span>
-									<span className="inline-block transition group-hover:translate-y-1">
-										{link.icon}
-									</span>
-								</Link>
-
-								{link.children?.length > 0 && (
-									<div
-										className={`absolute z-20 top-0 left-0 hidden group-hover:block hover:block`}
-									>
-										<Submenus menu={link} />
-									</div>
-								)}
-							</li>
-						);
-					})}
-				</ul>
-			</nav>
+			<Navbar openMenu={openMenu} onClose={onMenuClose} />
 
 			<SearchForm />
 
@@ -96,8 +36,8 @@ function Header() {
 					<span>Cart</span>
 				</Link>
 				<BsMenuApp
-					className="block lg:hidden"
-					onClick={() => setIsMenuOpen((prev) => !prev)}
+					className="block cursor-pointer md:hidden"
+					onClick={onMenuClose}
 				/>
 			</div>
 		</header>
