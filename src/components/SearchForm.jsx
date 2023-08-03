5b7262a488329de12b0ba3ead4f832e4ea4e71e8
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-04 01:01:05 +0530

    add value prop to search-form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { GoSearch } from "react-icons/go";
 
-function SearchForm({ searchQuery, className }) {
+function SearchForm ({ className }) {
+	const [searchValue, setSearchValue] = useState("");
+
 	return (
 		<div className={`p-2 hidden md:flex items-center rounded-full bg-gray-100`}>
 			<input
 				className="pl-2 flex-1 bg-transparent outline-none placeholder:text-gray-500"
 				type="search"
 				placeholder="Search Product"
+				value={searchValue}
+				onChange={(e) => setSearchValue(e.target.value)}
 			/>
 			<span className="p-1">{<GoSearch className="text-gray-600" />}</span>
 		</div>
