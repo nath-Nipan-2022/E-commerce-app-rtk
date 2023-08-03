20d4888c0533474090c56fa9d932a8c7e1c4969f
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-04 01:01:38 +0530

    add childrenClass prop for submenus

--- src/components/SubmenuLink.jsx
+++ src/components/SubmenuLink.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
-function Submenus({ menu, className }) {
+function Submenus({ menu, className,childrenClassName  }) {
 	return (
-		<ul className="w-full lg:w-56 p-2 transition duration-400 rounded-lg lg:bg-blue-50 lg:shadow font-medium">
+		<ul className={className}>
 			{menu.children?.map((item, i) => {
 				return (
 					<li key={i}>
 						<Link
-							to={menu.label + "/" + item}
-							className={`block p-1 lg:py-2 px-3 ${className}`}
+							to={menu.label.toLowerCase() + "/" + item.toLowerCase()}
+							className={`block p-1 lg:py-2 px-3 ${childrenClassName}`}
 						>
 							{item}
 						</Link>
