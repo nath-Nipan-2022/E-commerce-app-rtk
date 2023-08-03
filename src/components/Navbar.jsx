c4154c35757a90cb998762282e8c115f71b5ce19
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-04 01:03:32 +0530

    style nav menus :P

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,12 +40,12 @@ function Navbar({ openMenu, onClose }) {
 				></div>
 			)}
 			<div
-				className={`fixed z-20 left-0 top-0 w-52 md:w-auto h-screen md:h-auto md:static md:left-auto md:top-auto bg-gray-100 md:bg-transparent ${
+				className={`fixed z-20 left-0 top-0 w-52 lg:w-auto h-screen lg:h-auto lg:static lg:left-auto lg:top-auto bg-gray-100 lg:bg-transparent ${
 					openMenu ? "translate-x-0" : "-translate-x-full"
-				} md:translate-x-0 transition duration-300 rounded-lg border md:border-none shadow-lg md:shadow-none p-3 md:p-0`}
+				} lg:translate-x-0 transition duration-300 rounded-lg border lg:border-none shadow-lg lg:shadow-none p-3 lg:p-0`}
 			>
 				<ul
-					className={`lg:max-w-7xl flex flex-col lg:flex-row lg:items-center lg:gap-4 p-3 lg:p-0 lg:rounded-none lg:bg-transparent`}
+					className={`lg:max-w-7xl flex flex-col lg:flex-row lg:items-center lg:gap-4 p-3 lg:p-0 lg:rounded-none `}
 				>
 					{navLinks.map((link) => {
 						return (
@@ -55,30 +55,35 @@ function Navbar({ openMenu, onClose }) {
 								onClick={() => setOpenSubmenu((prev) => !prev)}
 							>
 								{!link.path ? (
-									<span className="block hover:bg-black/10 cursor-pointer p-1 px-2 rounded">
-										{link.label}
-									</span>
+									<div className="flex items-center justify-between hover:bg-black/5 lg:hover:bg-transparent cursor-pointer rounded">
+										<span className="p-1 px-2">{link.label}</span>
+										<span className="transition lg:group-hover:translate-y-1">
+											{link.icon}
+										</span>
+									</div>
 								) : (
 									<Link
 										to={link.path}
-										className="flex items-center gap-2 p-1 px-2 hover:bg-black/10 rounded lg:hover:bg-transparent lg:hover:text-slate-800"
+										className="flex items-center gap-2 p-1 px-2 hover:bg-black/5 rounded lg:hover:bg-transparent lg:hover:text-slate-800"
 									>
 										<span>{link.label}</span>
-										<span className="inline-block transition lg:group-hover:translate-y-1">
-											{link.icon}
-										</span>
 									</Link>
 								)}
 
 								{link.children?.length > 0 && (
 									<div
-										className={`lg:absolute z-20 top-full left-0 h-0 lg:h-auto transition ${
-											openSubmenu ? "visible h-32" : "invisible"
-										} hover:visible lg:group-hover:visible lg:group-hover:h-32`}
+										className={`lg:absolute z-20 top-full left-0 h-0 overflow-hidden lg:overflow-visible transition-all ${
+											openSubmenu ? "visible h-28" : "invisible"
+										} lg:invisible hover:visible lg:group-hover:visible lg:group-hover:h-36`}
 									>
 										<Submenus
 											menu={link}
-											className={"hover:bg-black/10 rounded"}
+											className={`w-full lg:w-56 p-2 lg:rounded-lg lg:bg-white lg:border lg:shadow-lg transition duration-300 ease-out ${
+												openSubmenu
+													? "opacity-100 translate-y-0"
+													: "opacity-0 translate-y-4"
+											} lg:opacity-100 lg:translate-y-0`}
+											childrenClassName={"hover:bg-black/5 rounded"}
 										/>
 									</div>
 								)}
