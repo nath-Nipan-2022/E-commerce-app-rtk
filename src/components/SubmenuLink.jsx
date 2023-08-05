2268bff7608993fa0ac397176621766d6519ed34
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-06 01:35:09 +0530

    style submenu properly

--- src/components/SubmenuLink.jsx
+++ src/components/SubmenuLink.jsx
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom";
 
-function Submenus({ menu, className,childrenClassName  }) {
+function Submenus({ menu, className, childrenClassName }) {
 	return (
 		<ul className={className}>
 			{menu.children?.map((item, i) => {
 				return (
-					<li key={i}>
+					<li key={i} className="mb-2">
 						<Link
-							to={menu.label.toLowerCase() + "/" + item.toLowerCase()}
-							className={`block p-1 lg:py-2 px-3 ${childrenClassName}`}
+							to={menu.label.toLowerCase() + "/" + item.label.toLowerCase()}
+							className={`block p-1 ${childrenClassName} bg-black/5 flex flex-col lg:flex-row items-center gap-2`}
 						>
-							{item}
+							<figure className="p-2 w-14 aspect-square hidden lg:block">
+								<img src={item.imageSrc} alt={item.altText} />
+							</figure>
+							<div className="p-2 lg:w-44">
+								<h3>{item.label}</h3>
+								<p className="text-sm text-gray-600">
+									{item.itemsAvailable} Items Available
+								</p>
+							</div>
 						</Link>
 					</li>
 				);
