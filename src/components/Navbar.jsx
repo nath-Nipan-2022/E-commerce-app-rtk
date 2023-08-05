c5d41db4f328f61c1b28355d44e61a1c1429b6e3
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-06 01:34:44 +0530

    Navbar update with awesome dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,51 @@ import Submenus from "./SubmenuLink";
 import { GoChevronDown } from "react-icons/go";
 import { useState } from "react";
 
+import demoImg from "../assets/slider/photo-1593359863503-f598684c806f-removebg-preview.png";
 function Navbar({ openMenu, onClose }) {
 	const navLinks = [
 		{
 			id: 1,
 			label: "Categories",
 			icon: <GoChevronDown />,
-			children: ["Men", "Women", "Kids"],
+			children: [
+				{
+					label: "Furniture",
+					itemsAvailable: 212,
+					imageSrc: demoImg,
+					altText: "Furniture",
+				},
+				{
+					label: "Shoe",
+					itemsAvailable: 212,
+					imageSrc: demoImg,
+					altText: "Shoe",
+				},
+				{
+					label: "Headphone",
+					itemsAvailable: 212,
+					imageSrc: demoImg,
+					altText: "Headphone",
+				},
+				{
+					label: "Laptop",
+					itemsAvailable: 212,
+					imageSrc: demoImg,
+					altText: "Laptop",
+				},
+				{
+					label: "Book",
+					itemsAvailable: 212,
+					imageSrc: demoImg,
+					altText: "Book",
+				},
+				{
+					label: "Handbag",
+					itemsAvailable: 212,
+					imageSrc: demoImg,
+					altText: "Handbag",
+				},
+			],
 		},
 		{
 			id: 2,
@@ -40,9 +78,9 @@ function Navbar({ openMenu, onClose }) {
 				></div>
 			)}
 			<div
-				className={`fixed z-20 left-0 top-0 w-52 lg:w-auto h-screen lg:h-auto lg:static lg:left-auto lg:top-auto bg-gray-100 lg:bg-transparent ${
-					openMenu ? "translate-x-0" : "-translate-x-full"
-				} lg:translate-x-0 transition duration-300 rounded-lg border lg:border-none shadow-lg lg:shadow-none p-3 lg:p-0`}
+				className={`fixed z-20 left-1/2 -translate-x-1/2 w-11/12 lg:w-auto lg:h-auto lg:static lg:left-auto lg:top-auto bg-gray-100 lg:bg-transparent ${
+					openMenu ? "translate-y-0 top-10" : "top-0 -translate-y-full"
+				} lg:translate-y-0 lg:translate-x-0 transition duration-300 rounded-lg border lg:border-none shadow-lg lg:shadow-none p-3 lg:p-0`}
 			>
 				<ul
 					className={`lg:max-w-7xl flex flex-col lg:flex-row lg:items-center lg:gap-4 p-3 lg:p-0 lg:rounded-none `}
@@ -72,14 +110,14 @@ function Navbar({ openMenu, onClose }) {
 
 								{link.children?.length > 0 && (
 									<div
-										className={`lg:absolute z-20 top-full left-0 lg:left-1/2 lg:-translate-x-1/2 ${
+										className={`lg:absolute z-20 top-full left-0 -translate-x-1/4 ${
 											openSubmenuOnClick ? "block" : "hidden"
 										} lg:hidden lg:group-hover:block `}
 									>
 										<Submenus
 											menu={link}
-											className={`w-full p-2 lg:w-56 lg:rounded-lg lg:bg-white lg:border lg:shadow-lg transition duration-300 opacity-0 -translate-x-2 animate-popUp`}
-											childrenClassName={"hover:bg-black/5 rounded"}
+											className={`w-full p-2 grid gap-4 grid-cols-2 lg:grid-cols-[200px_minmax(200px,_1fr)_200px] grid-flow-row lg:rounded-lg lg:bg-white lg:border lg:shadow-lg transition duration-300 opacity-0 -translate-x-2 animate-popUp`}
+											childrenClassName={"hover:bg-blue-600/10 rounded"}
 										/>
 									</div>
 								)}
