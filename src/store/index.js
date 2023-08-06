7c96702eb4a30ad2091134ca351093b4476d0d12
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-07 01:34:19 +0530

    create store for categories

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productsApi } from "../apis/productsApi";
+import { categoriesApi } from "../apis/categoriesApi";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 export const store = configureStore(
 	{
 		reducer: {
 			[productsApi.reducerPath]: productsApi.reducer,
+			[categoriesApi.reducerPath]: categoriesApi.reducer,
 		},
 		middleware: (getDefaultMiddleware) =>
-			getDefaultMiddleware().concat(productsApi.middleware),
+			getDefaultMiddleware()
+				.concat(productsApi.middleware)
+				.concat(categoriesApi.middleware),
 	}
 	// middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(photosApi.middleware),
 );
