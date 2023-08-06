c00a8d1e2330ac7722c7ebd3084f2c6fce21d23b
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-07 01:32:50 +0530

    style fully responsive dropdown

--- src/components/SubmenuLink.jsx
+++ src/components/SubmenuLink.jsx
@@ -8,14 +8,18 @@ function Submenus({ menu, className, childrenClassName }) {
 					<li key={i} className="mb-2">
 						<Link
 							to={menu.label.toLowerCase() + "/" + item.label.toLowerCase()}
-							className={`block p-1 ${childrenClassName} bg-black/5 flex flex-col lg:flex-row items-center gap-2`}
+							className={`block p-2 ${childrenClassName}`}
 						>
-							<figure className="p-2 w-14 aspect-square hidden lg:block">
-								<img src={item.imageSrc} alt={item.altText} />
+							<figure className="p-2 w-2/6 aspect-square hidden lg:block">
+								<img
+									src={item.imageSrc}
+									alt={item.altText}
+									className="scale-150 h-full object-cover"
+								/>
 							</figure>
-							<div className="p-2 lg:w-44">
-								<h3>{item.label}</h3>
-								<p className="text-sm text-gray-600">
+							<div className="lg:p-2 lg:w-44">
+								<h3 className="text-sm lg:text-base">{item.label}</h3>
+								<p className="text-xs lg:text-sm text-gray-700">
 									{item.itemsAvailable} Items Available
 								</p>
 							</div>
