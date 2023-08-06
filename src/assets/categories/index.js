aaf23e0471cd94e7773abc07289b5747eb37f0a1
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-07 01:30:16 +0530

    add some images for submenu/dropdown

---
+++ src/assets/categories/index.js
@@ -0,0 +1,8 @@
+import headphone from "./headphone.png";
+import book from "./book.png";
+import handbag from "./handbag.png";
+import furniture from "./furniture-sofa.png";
+import laptop from "./laptop.png";
+import nike_shoe from "./nike shoe.png";
+
+export { headphone, handbag, laptop, nike_shoe, book, furniture };
