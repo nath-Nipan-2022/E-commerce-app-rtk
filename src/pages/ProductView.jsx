5f9317d8a6fecfae7349c5c77f7d99ac2647ed60
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-09 18:21:19 +0530

    modified product view page

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,9 +1,26 @@
+import { useParams } from "react-router-dom";
 import ProductDetails from "../components/ProductDetails";
+import { useGetProductAlbumQuery } from "../apis/productAlbumsApi";
+import ProductViewSkeleton from "../components/ProductViewSkeleton";
 
 function ProductView() {
+	const { id } = useParams();
+	const { data, isLoading, error } = useGetProductAlbumQuery(id);
+
 	return (
-		<div>
-			<ProductDetails />
+		<div className="mx-auto">
+			<section className="border-t p-3 px-10">
+				<span className="font-medium text-gray-600 text-xs">{`/Product/${
+					data ? data[0].name : id
+				}`}</span>
+			</section>
+			{isLoading ? <ProductViewSkeleton /> : <ProductDetails product={data} />}
+
+			{error && (
+				<div className="text-lg text-red-700 p-4 rounded-md bg-red-50">
+					Error loading product!
+				</div>
+			)}
 		</div>
 	);
 }
