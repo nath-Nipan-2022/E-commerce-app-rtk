675fa4c2564a744d24056ea83e6952f2979661cb
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-11 01:57:25 +0530

    update styles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import Button from "./Button";
+// import Button from "./Button";
 import img1 from "../assets/slider/photo-1623682490567-941cdfca10a4-removebg-preview.png";
 import img2 from "../assets/slider/photo-1593359863503-f598684c806f-removebg-preview.png";
 import img3 from "../assets/slider/photo-1545127398-14699f92334b-removebg-preview.png";
 import img4 from "../assets/slider/photo-1625786682948-2168238883d2-removebg-preview.png";
+import { Link } from "react-router-dom";
 
 function Slider() {
 	const slides = [
@@ -30,33 +31,32 @@ function Slider() {
 	const renderSlides = slides.map((slide) => (
 		<div
 			key={slide.id}
-			className="flex justify-around items-center h-64"
+			className="flex justify-evenly items-center"
 			style={{ width: 100 / slides.length + "%" }}
 		>
-			<div className="p-12">
-				<h1 className="font-bold text-2xl md:text-3xl lg:text-4xl text-slate-700 mb-5">
+			<div className="p-3 md:p-6">
+				<h3 className="font-bold text-lg leading-6 sm:text-3xl lg:text-4xl text-slate-700 mb-3 md:mb-6">
 					Grab Upto 50% Off On
 					<br />
 					Selected Products
-				</h1>
-				<Button
-					className={
-						"px-6 h-11 bg-slate-800 text-white font-medium rounded-full hover:bg-slate-700 transition-colors"
-					}
+				</h3>
+				<Link
+					to={"/"}
+					className="inline-block rounded-full py-2 px-4 bg-slate-800 text-white text-xs sm:text-base sm:px-6 font-medium hover:bg-slate-700 transition"
 				>
 					Buy Now
-				</Button>
+				</Link>
 			</div>
 
-			<figure className="w-56 pr-10">
-				<img src={slide.src} alt={slide.alt} />
+			<figure className="w-1/4 lg:p-4">
+				<img className="h-full" src={slide.src} alt={slide.alt} />
 			</figure>
 		</div>
 	));
 
 	return (
 		<div
-			className="flex transition duration-1000"
+			className="flex transition duration-1000 max-h-64"
 			style={{
 				width: slides.length * 100 + "%",
 				transform: `translateX(-${currentSlide * (100 / slides.length)}%)`,
