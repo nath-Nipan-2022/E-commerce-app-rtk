8414bb7d1c9fd16b56d0a6c6d9667d0325d0ae8f
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-11 01:56:47 +0530

    Add Category section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Category from "../components/Category";
 import ProductsList from "../components/ProductsList";
 import Slider from "../components/Slider";
 // import { useState } from "react";
@@ -5,13 +6,22 @@ import Slider from "../components/Slider";
 function Home() {
 	return (
 		<div className="mt-4">
-			<section className=" bg-rose-100 mx-auto rounded max-w-2xl lg:max-w-7xl overflow-hidden">
+			<section className=" bg-rose-100 mx-auto rounded-lg max-w-2xl lg:max-w-7xl overflow-hidden">
 				<Slider />
 			</section>
 
 			<section className="mx-auto max-w-2xl py-16 lg:max-w-7xl">
 				<h2 className="text-2xl font-medium mb-8">Trending Products For You</h2>
-				<ProductsList />
+				<ProductsList type='trending' />
+			</section>
+
+			<section>
+				<Category className="mx-auto rounded max-w-2xl lg:max-w-7xl" />
+			</section>
+
+			<section className="mx-auto max-w-2xl py-16 lg:max-w-7xl">
+				<h2 className="text-2xl font-medium mb-8">Most Bought Products</h2>
+				<ProductsList type="popular" />
 			</section>
 		</div>
 	);
