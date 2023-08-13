9cb2844e47184b1af59e44c2ef8f2943d352ae2d
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-13 23:14:54 +0530

    change ui, wrap dispatches & import Counter.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,30 +1,59 @@
-import { GoTrash } from "react-icons/go";
+import { GoX } from "react-icons/go";
+import { useDispatch } from "react-redux";
+import {
+	incrementQuantity,
+	decrementQuantity,
+	removeCart,
+} from "../store/slices/cartsSlice";
+import Counter from "./Counter";
 
-function Cart({ className, item, onRemove }) {
+function Cart({ className, cartItem }) {
+	const { name, price, quantity, images, altText } = cartItem;
+
+	const dispatch = useDispatch();
+
+	const increment = () => {
+		dispatch(incrementQuantity(cartItem)); // <- action object is different
+	};
+
+	const decrement = () => {
+		dispatch(decrementQuantity(cartItem)); // <- action object is different
+	};
+
 	const handleRemove = () => {
-		console.log(item);
-		onRemove(item);
+		dispatch(removeCart(cartItem));
 	};
 
 	return (
-		<div className={`flex items-center gap-1 ${className}`}>
-			<figure className="w-12 h-12">
+		<div className={`flex items-center gap-4 ${className}`}>
+			<figure className="w-14 h-16 rounded overflow-hidden">
 				<img
-					src={item.imageUrl}
-					alt={item.altText}
+					src={images[0]}
+					alt={altText}
 					className="w-full h-full object-cover"
 				/>
 			</figure>
-			<article className="flex-1 flex flex-col gap-1 justify-between">
-				<h3 className="text-gray-600">{item.name}</h3>
-				<p className="font-bold text-slate-600">${item.price}</p>
+			<article className="flex-1">
+				<h3 className="mb-2 text-gray-700 flex gap-1 justify-between">
+					{name}
+					<div
+						onClick={handleRemove}
+						className="w-6 h-6 grid place-items-center cursor-pointer group"
+					>
+						<GoX className="text-gray-500 group-hover:text-gray-900" />
+					</div>
+				</h3>
+				<div className="mt-2 flex gap-2 justify-between items-center">
+					{/* counter*/}
+					<Counter
+						className={"border rounded-md"}
+						count={quantity}
+						onIncrement={increment}
+						onDecrement={decrement}
+					/>
+					<p>${price}</p>
+				</div>
 			</article>
-			<div
-				onClick={handleRemove}
-				className="p-2 w-8 h-8 bg-gray-100 hover:bg-red-200 rounded-lg relative"
-			>
-				<GoTrash className="absolute top-1/2 -translate-y-1/2" />
-			</div>
 		</div>
 	);
 }
