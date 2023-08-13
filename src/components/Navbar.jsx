676dd50b9c2651d8a3fadec5351e44a20051d3d8
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-13 23:25:18 +0530

    add images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,15 @@ import Submenus from "./SubmenuLink";
 import { GoChevronDown } from "react-icons/go";
 import { useState } from "react";
 
-import demoImg from "../assets/slider/photo-1593359863503-f598684c806f-removebg-preview.png";
+import {
+	headphone,
+	book,
+	nike_shoe,
+	furniture,
+	handbag,
+	laptop,
+} from "../assets/categories";
+
 function Navbar({ openMenu, onClose }) {
 	const navLinks = [
 		{
@@ -14,37 +22,37 @@ function Navbar({ openMenu, onClose }) {
 				{
 					label: "Furniture",
 					itemsAvailable: 212,
-					imageSrc: demoImg,
+					imageSrc: furniture,
 					altText: "Furniture",
 				},
 				{
 					label: "Shoe",
 					itemsAvailable: 212,
-					imageSrc: demoImg,
+					imageSrc: nike_shoe,
 					altText: "Shoe",
 				},
 				{
 					label: "Headphone",
 					itemsAvailable: 212,
-					imageSrc: demoImg,
+					imageSrc: headphone,
 					altText: "Headphone",
 				},
 				{
 					label: "Laptop",
 					itemsAvailable: 212,
-					imageSrc: demoImg,
+					imageSrc: laptop,
 					altText: "Laptop",
 				},
 				{
 					label: "Book",
 					itemsAvailable: 212,
-					imageSrc: demoImg,
+					imageSrc: book,
 					altText: "Book",
 				},
 				{
 					label: "Handbag",
 					itemsAvailable: 212,
-					imageSrc: demoImg,
+					imageSrc: handbag,
 					altText: "Handbag",
 				},
 			],
@@ -69,18 +77,21 @@ function Navbar({ openMenu, onClose }) {
 	const [openSubmenuOnClick, setOpenSubmenuOnClick] = useState(false);
 
 	return (
-		<nav className={`h-[56px] flex items-center`}>
+		<nav className={`absolute lg:relative h-[56px] flex items-center`}>
 			{/* Overlay */}
 			{openMenu && (
 				<div
-					className={"fixed z-10 inset-0 bg-black/10 md:hidden"}
+					className={"fixed z-20 inset-0 bg-black/10 lg:hidden"}
 					onClick={onClose}
 				></div>
 			)}
+			{/* The menu appear on mobile */}
 			<div
-				className={`fixed z-20 left-1/2 -translate-x-1/2 w-11/12 lg:w-auto lg:h-auto lg:static lg:left-auto lg:top-auto bg-gray-100 lg:bg-transparent ${
-					openMenu ? "translate-y-0 top-10" : "top-0 -translate-y-full"
-				} lg:translate-y-0 lg:translate-x-0 transition duration-300 rounded-lg border lg:border-none shadow-lg lg:shadow-none p-3 lg:p-0`}
+				className={`fixed z-20 left-1/2 -translate-x-1/2 w-11/12 lg:w-auto lg:h-auto lg:static lg:left-auto lg:top-auto bg-gray-100 lg:bg-transparent transition duration-300 ${
+					openMenu
+						? "top-10 opacity-100 translate-y-0"
+						: "top-0 opacity-0 -translate-y-full"
+				} lg:opacity-100 lg:translate-y-0 lg:translate-x-0 rounded-lg border lg:border-none shadow-lg lg:shadow-none p-3 lg:p-0`}
 			>
 				<ul
 					className={`lg:max-w-7xl flex flex-col lg:flex-row lg:items-center lg:gap-4 p-3 lg:p-0 lg:rounded-none `}
@@ -100,24 +111,26 @@ function Navbar({ openMenu, onClose }) {
 										<span>{link.label}</span>
 									</Link>
 								) : (
-									<div className="flex items-center justify-between hover:bg-black/5 lg:hover:bg-transparent cursor-pointer rounded">
+									<div className="flex items-center justify-between cursor-pointer rounded">
 										<span className="p-1 px-2">{link.label}</span>
-										<span className="lg:group-hover:translate-y-1">
+										<span className="group-hover:text-blue-600 lg:group-hover:translate-y-1">
 											{link.icon}
 										</span>
 									</div>
 								)}
-
+								{/* dropdown  */}
 								{link.children?.length > 0 && (
 									<div
-										className={`lg:absolute z-20 top-full left-0 -translate-x-1/4 ${
+										className={`lg:absolute z-20 top-full left-0 pt-1 lg:pt-4 ${
 											openSubmenuOnClick ? "block" : "hidden"
-										} lg:hidden lg:group-hover:block `}
+										} lg:hidden lg:group-hover:block`}
 									>
 										<Submenus
 											menu={link}
-											className={`w-full p-2 grid gap-4 grid-cols-2 lg:grid-cols-[200px_minmax(200px,_1fr)_200px] grid-flow-row lg:rounded-lg lg:bg-white lg:border lg:shadow-lg transition duration-300 opacity-0 -translate-x-2 animate-popUp`}
-											childrenClassName={"hover:bg-blue-600/10 rounded"}
+											className={`border-t w-full p-1 lg:p-2 grid gap-x-2 grid-cols-2 lg:grid-cols-dropdown grid-flow-row lg:rounded-lg lg:bg-white lg:border lg:shadow-lg transition duration-300 opacity-0 -translate-y-2 lg:translate-y-0 lg:-translate-x-2 animate-popUp`}
+											childrenClassName={
+												"lg:bg-gray-500/5 lg:flex lg:flex-row lg:items-center gap-2 hover:bg-blue-200 rounded"
+											}
 										/>
 									</div>
 								)}
