2f889d394ca236cf0c9de7ece29a58972a933c79
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-13 23:22:21 +0530

    add 'increment & decrement' reducers

--- src/store/slices/cartsSlice.js
+++ src/store/slices/cartsSlice.js
@@ -4,23 +4,34 @@ const CartsSlice = createSlice({
 	name: "carts",
 	initialState: {
 		list: [],
-		isOpen: false,
+		// isOpen: false
 	},
 	reducers: {
 		addCart(state, action) {
 			// add a cart
-			// console.log(action);
-			state.list.push(action.payload);
+			// increase quantity if adding repeatedly
+			const hasItem = state.list.find((item) => item.id === action.payload.id);
+			if (hasItem) {
+				hasItem.quantity = action.payload.quantity;
+			} else {
+				state.list.push(action.payload);
+			}
+		},
+		incrementQuantity(state, action) {
+			const hasItem = state.list.find((item) => item.id === action.payload.id);
+			hasItem.quantity = hasItem.quantity + 1;
+		},
+		decrementQuantity(state, action) {
+			const hasItem = state.list.find((item) => item.id === action.payload.id);
+			hasItem.quantity = hasItem.quantity - 1;
 		},
 		removeCart(state, action) {
 			// remove a cart
 			state.list = state.list.filter(({ id }) => id !== action.payload.id);
 		},
-		openCart(state, action) {
-			state.isOpen = true;
-		},
 	},
 });
 
-export const { addCart, removeCart } = CartsSlice.actions;
+export const { addCart, removeCart, incrementQuantity, decrementQuantity } =
+	CartsSlice.actions;
 export const CartsReducer = CartsSlice.reducer;
