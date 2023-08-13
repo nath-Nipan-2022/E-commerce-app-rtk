440a8efc3b8e627f52a66fa5b19cca941ba13e26
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-13 23:17:12 +0530

    toggle cartList component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,63 +6,56 @@ import SearchForm from "./SearchForm";
 import logo from "../assets/shopping logo.png";
 import { useState } from "react";
 import Navbar from "./Navbar";
+import Modal from "./Modal";
+import CartList from "./CartList";
+import { useSelector } from "react-redux";
 
 function Header() {
 	const [openMenu, setOpenMenu] = useState(false);
-	const [cartQuantity, setCartQuantity] = useState(2);
-
-	const linksWithIcon = [
-		{
-			path: "/account",
-			label: "Account",
-			icon: <GoPerson />,
-		},
-		{
-			path: "/cart",
-			label: "Cart",
-			icon: (
-				<div className="relative">
-					<BsCart2 />{" "}
-					<span className="absolute -top-1/2 -right-1/2 w-4 h-4 font-bold text-[10px] grid place-items-center rounded-full bg-blue-600 text-white">
-						{cartQuantity}
-					</span>
-				</div>
-			),
-		},
-	];
+	const [openCartList, setOpenCartList] = useState(false);
+	const cartQuantity = useSelector((state) => state.carts.list.length);
 
 	const onMenuClose = () => {
 		setOpenMenu((prev) => !prev);
 	};
 
 	return (
-		<header className="flex justify-between md:justify-evenly lg:justify-between items-center p-2">
-			<div>
-				<Link to={"/"} className="flex items-center font-medium">
-					<img src={logo} alt="company logo" width={56} />
-					<span className="text-xl font-medium">ShopCart</span>
-				</Link>
-			</div>
-
-			<Navbar openMenu={openMenu} onClose={onMenuClose} />
-
-			<SearchForm className={"sm:w-60 ml-auto sm:ml-0 lg:w-auto"} />
-
-			<div className="flex items-center gap-4 font-medium">
-				{linksWithIcon.map((link) => (
+		<header className="sticky top-0 z-10 bg-white">
+			<div className="max-w-7xl mx-auto px-6 flex justify-between md:justify-evenly lg:justify-between items-center p-2">
+				<div>
+					<Link to={"/"} className="flex items-center font-medium">
+						<img src={logo} alt="company logo" width={56} />
+						<span className="text-xl font-medium">ShopCart</span>
+					</Link>
+				</div>
+				<Navbar openMenu={openMenu} onClose={onMenuClose} />
+				<SearchForm className={"sm:w-60 ml-auto sm:ml-0 lg:w-auto"} />
+				<div className="flex items-center gap-4 font-medium">
 					<Link
-						to={link.path}
-						key={link.label}
+						to={"/account"}
 						className="flex items-center gap-2 p-1 px-1.5 rounded"
 					>
-						{link.icon}
-						<span className="hidden lg:inline-block">{link.label}</span>
+						<GoPerson />
+						<span className="hidden lg:inline-block">Account</span>
 					</Link>
-				))}
-				<FiMenu
-					className="block cursor-pointer lg:hidden ml-1"
-					onClick={onMenuClose}
-				/>
+					<FiMenu
+						className="block cursor-pointer lg:hidden ml-1"
+						onClick={onMenuClose}
+					/>
+					<div
+						className="flex items-center gap-2 p-1 px-1.5 rounded  cursor-pointer"
+						onClick={() => setOpenCartList(true)}
+					>
+						<div className="relative">
+							<BsCart2 />
+							<span className="absolute -top-1/2 -right-1/2 w-4 h-4 font-bold text-[10px] grid place-items-center rounded-full bg-blue-600 text-white">
+								{cartQuantity}
+							</span>
+						</div>
+						<span>Cart</span>
+					</div>
+				</div>
+				{openCartList && <CartList onClose={() => setOpenCartList(false)} />}
 			</div>
 		</header>
 	);
