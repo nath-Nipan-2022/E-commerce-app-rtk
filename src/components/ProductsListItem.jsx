94b978fd10921e760323ee89ad2daed1d933fced
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-17 23:59:47 +0530

    update

--- src/components/ProductsListItem.jsx
+++ src/components/ProductsListItem.jsx
@@ -4,8 +4,11 @@ import Button from "./Button";
 import Wishlist from "./Wishlist";
 import { useDispatch } from "react-redux";
 import { addCart } from "../store";
+import ProductImage from "./ProductImage";
 
 function ProductsListItem({ product }) {
+	const { name, price, images } = product.attributes;
+
 	const [quantity, setQuantity] = useState(1);
 	const dispatch = useDispatch();
 
@@ -23,22 +26,16 @@ function ProductsListItem({ product }) {
 	return (
 		<div className="relative group">
 			<Link to={`products/${product.id}`}>
-				<figure className="aspect-square overflow-hidden rounded-lg bg-gray-200">
-					<img
-						src={product.images[0]}
-						alt={product.altText}
-						className="h-full w-full object-cover object-center transition group-hover:scale-105"
-					></img>
-				</figure>
+				<ProductImage product={product} url={images.data[0].attributes.url} />
 				<article className="font-medium">
-					<h3 className="text-lg mt-1 text-gray-700">{product.name}</h3>
-					<p className="text-lg mt-2 lg:mt-4 pb-1 sm:text-xl text-slate-700">
-						${product.price}
+					<h3 className="text-lg my-2 text-gray-700">{name}</h3>
+					<p className="text-lg mb-2 md:text-xl text-slate-700">
+						${price}
 					</p>
 				</article>
 			</Link>
 			<Button
-				className="absolute right-0 bottom-0 hover:bg-slate-900 hover:border-slate-900 hover:text-white py-1.5 px-3 md:px-4 text-xs lg:text-base rounded-lg border border-gray-300"
+				className="mt-2 hover:bg-slate-900 hover:border-slate-900 hover:text-white py-1.5 px-3 sm:px-4 text-xs lg:text-base rounded-lg border border-gray-300"
 				onClick={addToCart}
 			>
 				Add to Cart
@@ -47,4 +44,5 @@ function ProductsListItem({ product }) {
 		</div>
 	);
 }
+
 export default ProductsListItem;
