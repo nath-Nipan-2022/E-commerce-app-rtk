a9ab818a96587a65cba54b97be3dbebf3083ebc7
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-19 23:25:54 +0530

    update product fetching and styles

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,20 +1,20 @@
 import { useParams } from "react-router-dom";
 import ProductDetails from "../components/ProductDetails";
-import { useGetProductAlbumQuery } from "../apis/productAlbumsApi";
 import ProductViewSkeleton from "../components/ProductViewSkeleton";
+import { useGetProductByIdQuery } from "../store/apis/productsApi";
 
 function ProductView() {
 	const { id } = useParams();
-	const { data, isLoading, error } = useGetProductAlbumQuery(id);
+	const { data, isLoading, error } = useGetProductByIdQuery(`/${id}?populate=*`);
 
 	return (
 		<div className="mx-auto">
-			<section className="border-t p-3 px-10">
+			{/* <section className="border-t p-3 px-10">
 				<span className="font-medium text-gray-600 text-xs">{`/Product/${
 					data ? data[0].name : id
 				}`}</span>
-			</section>
-			{isLoading ? <ProductViewSkeleton /> : <ProductDetails product={data} />}
+			</section> */}
+			{isLoading ? <ProductViewSkeleton /> : <ProductDetails product={data.data} />}
 
 			{error && (
 				<div className="text-lg text-red-700 p-4 rounded-md bg-red-50">
