f598c007a2ae1a00086aa11f86fe953f6d2afde8
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-20 23:02:05 +0530

    fetch by  a prop 'type', done but not committed

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,19 +1,18 @@
-import { useGetProductsQuery } from "../apis/productsApi";
-
+import { useGetProductsQuery } from "../store/apis/productsApi";
 import ProductsListItem from "./ProductsListItem";
 import Skeleton from "./Skeleton";
 
-function ProductsList() {
-	const { data, isLoading, error } = useGetProductsQuery();
+function ProductsList ({ type }) {
+	const { data, isLoading, error } = useGetProductsQuery(`?populate=*&filters[type][$eq]=${type}`);
 
-	const renderProducts = data?.map((product) => {
+	const renderProducts = data?.data.map((product) => {
 		return <ProductsListItem key={product.id} product={product} />;
 	});
 
 	return (
-		<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8">
+		<div className="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8">
 			{isLoading ? (
-				<Skeleton times={4} className={"h-60 m-3 rounded-md"} />
+				<Skeleton times={8} className={"h-44 md:h-52 m-2 rounded-md"} />
 			) : (
 				renderProducts
 			)}
