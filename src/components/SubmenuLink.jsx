0fc83e5dae5c0d5d750af81d8742dcff78d8b8bd
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-23 23:45:43 +0530

    update categories on nav-link's hover.

--- src/components/SubmenuLink.jsx
+++ src/components/SubmenuLink.jsx
@@ -1,33 +1,41 @@
 import { Link } from "react-router-dom";
+import { useGetCategoriesQuery } from "../store/apis/categoriesApi";
+import Skeleton from "./Skeleton";
+import ProductImage from "./ProductImage";
 
-function Submenus({ menu, className, childrenClassName }) {
-	return (
-		<ul className={className}>
-			{menu.children?.map((item, i) => {
-				return (
-					<li key={i}>
-						<Link
-							to={menu.label.toLowerCase() + "/" + item.label}
-							className={`block p-2 ${childrenClassName}`}
-						>
-							<figure className="p-2 w-20 aspect-square hidden lg:block"> 
-								<img
-									src={item.imageSrc}
-									alt={item.altText}
-									className="h-full w-full object-cover"
-								/>
-							</figure>
-							<div className="flex flex-col justify-between sm:flex-row sm:pr-8 lg:flex-col lg:p-2 lg:w-44">
-								<h6 className="text-sm lg:text-base">{item.label}</h6>
-								<p className="text-xs lg:text-sm text-slate-500">
-									{item.itemsAvailable} Items Available
-								</p>
-							</div>
-						</Link>
-					</li>
-				);
-			})}
-		</ul>
+function Submenus({ className, childrenClassName }) {
+	const { data: cats } = useGetCategoriesQuery(
+		"?populate=*populate[0]=products&populate[1]=products.images"
 	);
+
+	const renderCats = cats?.data.map((item, i) => {
+		if (i > 5) {
+			return;
+		}
+		const images = item.attributes.products.data[0].attributes.images.data;
+
+		return (
+			<li key={item.id}>
+				<Link
+					to={`/categories/${item.attributes.name}`}
+					className={`block p-2 ${childrenClassName}`}
+				>
+					<ProductImage
+						url={images[0].attributes.url}
+						alt={item.desc}
+						className="w-16 hidden lg:block"
+					/>
+					<div className="flex flex-col justify-between items-center lg:items-start gap-2 sm:flex-row sm:pr-8 lg:flex-col lg:p-2 lg:w-44">
+						<h6 className="text-sm lg:text-base">{item.attributes.name}</h6>
+						<p className="text-xs lg:text-sm text-slate-500">
+							{Math.floor(Math.random() * 200) + 10} Items Available
+						</p>
+					</div>
+				</Link>
+			</li>
+		);
+	});
+
+	return <ul className={className}>{renderCats}</ul>;
 }
 export default Submenus;
