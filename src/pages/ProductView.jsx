bf125acbf9cc091a20923c0a38390eb6734c5c6a
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-23 23:59:00 +0530

    show skeleton on isFetching state.

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -5,16 +5,17 @@ import { useGetProductByIdQuery } from "../store/apis/productsApi";
 
 function ProductView() {
 	const { id } = useParams();
-	const { data, isLoading, error } = useGetProductByIdQuery(`/${id}?populate=*`);
+	const { data, isLoading, isFetching, error } = useGetProductByIdQuery(
+		`/${id}?populate=*`
+	);
 
 	return (
 		<div className="mx-auto">
-			{/* <section className="border-t p-3 px-10">
-				<span className="font-medium text-gray-600 text-xs">{`/Product/${
-					data ? data[0].name : id
-				}`}</span>
-			</section> */}
-			{isLoading ? <ProductViewSkeleton /> : <ProductDetails product={data.data} />}
+			{isLoading || isFetching ? (
+				<ProductViewSkeleton />
+			) : (
+				<ProductDetails product={data.data} />
+			)}
 
 			{error && (
 				<div className="text-lg text-red-700 p-4 rounded-md bg-red-50">
