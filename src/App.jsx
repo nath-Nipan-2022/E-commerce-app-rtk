f3f8790fa708737434df367f1a3d8c71fae2707d
Nipan Debnath <debnathnipan719@gmail.com>
2023-08-24 23:41:51 +0530

    Folder structure changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,55 @@
-import { useEffect } from "react";
-import Header from "./components/Header";
-import Home from "./pages/Home.jsx";
-import Account from "./pages/Account";
-import ErrorPage from "./pages/ErrorPage.jsx";
-import { RouterProvider, Outlet, createBrowserRouter ,useLocation} from "react-router-dom";
-import ProductView from "./pages/ProductView";
+import { RouterProvider, Outlet, createBrowserRouter } from "react-router-dom";
+import { Home, Account, ErrorPage, ProductView, CategoriesPage } from "./pages";
 import Footer from "./components/Footer";
-import CategoriesPage from "./pages/CategoriesPage";
+import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
+import { ScrollToTop } from "./helper";
 
 const Layout = () => {
-	return (
-		<div className="font-poppins">
-			<Header />
-			<main className="max-w-7xl mx-auto px-6">
-				<ScrollToTop />
-				<Outlet />
-			</main>
-			<Footer />
-		</div>
-	);
-};
-
-const ScrollToTop = () => {
-	const { pathname } = useLocation();
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, [pathname]);
-	// Automatically scrolls to top whenever pathname changes
+  return (
+    <div className="font-poppins">
+      <Header />
+      <main className="max-w-7xl mx-auto px-6">
+        <ScrollToTop />
+        <Outlet />
+        <Toaster position="bottom-center" />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Layout />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/account",
-				element: <Account />,
-			},
-			{
-				path: "/categories/:id",
-				element: <CategoriesPage />,
-			},
-			{
-				path: "/categories/:id/products/:id",
-				element: <ProductView />,
-			},
-			{
-				path: "/products/:id",
-				element: <ProductView />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/account",
+        element: <Account />,
+      },
+      {
+        path: "/categories/:id",
+        element: <CategoriesPage />,
+      },
+      {
+        path: "/categories/:id/products/:id",
+        element: <ProductView />,
+      },
+      {
+        path: "/products/:id",
+        element: <ProductView />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-	return <RouterProvider router={router}></RouterProvider>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 export default App;
