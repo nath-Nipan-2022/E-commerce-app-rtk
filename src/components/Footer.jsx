5e4b92178a073218995d0bd8be17062a755c5dce
Nipan Debnath <debnathnipan719@gmail.com>
2023-09-24 23:50:37 +0530

    styled footer properly.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,44 +3,44 @@ import { footerLinks } from "../constants";
 
 function Footer() {
   return (
-    <div className="py-2 px-6 mb-4 lg:max-w-7xl mx-auto">
-      {/* <!-- Top section --> */}
-      <div className="mt-20 grid grid-cols-2 sm:grid-cols-4 gap-10">
-        {footerLinks.map((link) => (
-          <div
-            className="flex-1 flex flex-col gap-4 text-justify text-sm"
-            key={link.title}
-          >
-            <h1 className="text-base font-semibold text-gray-700">
-              {link.title}
-            </h1>
-            {link.links?.map((link) => (
-              <span key={link} className="text-gray-500">
-                {link}
-              </span>
-            ))}
-            {link.desc && <span className="text-gray-500">{link.desc}</span>}
-          </div>
-        ))}
-      </div>
-
-      {/* <!-- Bottom section --> */}
-      <div className="mt-10 flex items-center justify-between">
-        <div className="flex items-center">
-          <div className="text-blue-600 text-xl">ShopCart</div>
-          <div className="text-gray-600 text-sm ml-4">@Copyright 2023</div>
+    <footer className="mt-16 bg-slate-900">
+      <div className="px-6 pt-20 pb-10 mx-auto lg:max-w-7xl">
+        {/* <!-- Top section --> */}
+        <div className="grid grid-cols-2 gap-10 md:grid-cols-4">
+          {footerLinks.map((link) => (
+            <div className="flex flex-col flex-1 gap-4" key={link.title}>
+              <h6 className="font-semibold text-slate-300">{link.title}</h6>
+              {link?.links && (
+                <ul className="pl-4 text-sm list-disc">
+                  {link.links.map((link) => (
+                    <li key={link} className="py-1 text-slate-400">
+                      {link}
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {link.desc && (
+                <p className="text-sm text-slate-400">{link.desc}</p>
+              )}
+            </div>
+          ))}
         </div>
-        <div>
+        {/* <!-- Bottom section --> */}
+        <div className="grid items-center grid-cols-2 gap-10 mt-10">
+          <div className="flex flex-col sm:flex-row sm:gap-4 sm:items-center">
+            <span className="text-2xl font-bold text-blue-600">ShopCart</span>
+            <span className="text-sm text-gray-400">@Copyright 2023</span>
+          </div>
           <img
-            className="h-10 w-full"
             src={paymentLogos}
             alt="payment logos"
             width={100}
             height={44}
+            className="w-full ml-auto md:w-[calc(50%-20px)]"
           />
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 export default Footer;
