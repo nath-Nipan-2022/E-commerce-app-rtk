0d5a9f1f39d953a607ddae6c7c6c09ce424e3818
Nipan Debnath <debnathnipan719@gmail.com>
2023-09-24 23:59:41 +0530

    used UrlSearchParams

--- src/components/Rating/RatingStarsInputs.jsx
+++ src/components/Rating/RatingStarsInputs.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { GoStarFill } from "react-icons/go";
+import { useSearchParams } from "react-router-dom";
 
 function RatingStars({ onChange }) {
-  const [review, setReview] = useState(3);
   const [isChecked, setIsChecked] = useState(false);
+  const [searchParams] = useSearchParams();
+  const selectedRatings = searchParams.get("ratings-above") ?? 3;
 
   const handleRatingsChange = (i) => {
-    setReview(i);
-    isChecked ? onChange(i + 1) : onChange(1);
-    console.log("checked", isChecked);
+    isChecked ? onChange(i + 1) : onChange(0);
+    console.log(isChecked, selectedRatings, i);
   };
 
   const renderReviews = Array(5)
@@ -18,7 +19,7 @@ function RatingStars({ onChange }) {
         key={i}
         onClick={() => handleRatingsChange(i)}
         className={`transition-colors ${
-          i <= review ? "text-yellow-500" : "text-gray-300"
+          i <= selectedRatings - 1 ? "text-yellow-500" : "text-gray-300"
         }`}
       />
     ));
@@ -26,7 +27,7 @@ function RatingStars({ onChange }) {
   return (
     <article className="p-4 border border-b-0">
       <h3 className="font-medium">Rating</h3>
-      <div className="flex gap-2 items-center my-2 group cursor-pointer">
+      <div className="flex items-center gap-2 my-2 cursor-pointer group">
         <input
           type="checkbox"
           id="checkbox-rating"
@@ -35,7 +36,7 @@ function RatingStars({ onChange }) {
         />
         {renderReviews}
         <label
-          className="text-gray-600 group-hover:text-gray-800 flex-1 cursor-pointer"
+          className="flex-1 text-gray-600 cursor-pointer group-hover:text-gray-800"
           htmlFor="checkbox-rating"
         >
           above
