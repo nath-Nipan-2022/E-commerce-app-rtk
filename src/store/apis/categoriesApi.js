f677c4d089bd1f987ce53c1df2fb2c9ee13b0714
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-01 23:51:37 +0530

    updated api

--- src/store/apis/categoriesApi.js
+++ src/store/apis/categoriesApi.js
@@ -1,45 +1,43 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const pause = async (duration) => {
-	return new Promise((resolve) => {
-		setTimeout(() => resolve(), duration);
-	});
-};
-
 const categoriesApi = createApi({
-	reducerPath: "category",
-	tagTypes: ["Category"],
+  reducerPath: "category",
+  tagTypes: ["Category"],
 
-	baseQuery: fetchBaseQuery({
-		baseUrl: import.meta.env.VITE_REACT_APP_API_URL,
-		prepareHeaders: (headers) => {
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_REACT_APP_API_URL,
+    prepareHeaders: (headers) => {
 			headers.set('authorization', 'Bearer ' + import.meta.env.VITE_REACT_APP_API_TOKEN);
-			return headers;
-		},
-		fetchFn: async (...args) => {
-			await pause(500);
-			return fetch(...args);
-		},
-	}),
-	endpoints: (builder) => ({
-		getCategories: builder.query({
-			query: (query) => `/categories/${query}`,
-			providesTags: (results, error, category) => {
-				const tags = results.data.map((category) => ({
-					type: "categories",
-					id: category.id,
-				}));
-				return tags;
-			},
-		}),
-		getACategory: builder.query({
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getCategories: builder.query({
+      query: (query) => `/categories/${query}`,
+      providesTags: (results, error, category) => {
+        const tags = results.data.map((category) => ({
+          type: "categories",
+          id: category.id,
+        }));
+        return tags;
+      },
+    }),
+    getACategory: builder.query({
 			query: (category) => `/products/${category}`
-		}),
-		getSubcategories: builder.query({
-			query: (category) => `/sub-categories/${category}`
-		})
-	}),
+    }),
+    getSubcategories: builder.query({
+      query: (category) => `/sub-categories/${category}`
+    }),
+    getColors: builder.query({
+      query: (category) => `/colors/${category}`,
+    }),
+  }),
 });
 
 export { categoriesApi };
-export const { useGetCategoriesQuery,useGetACategoryQuery, useGetSubcategoriesQuery } = categoriesApi;
+export const {
+  useGetCategoriesQuery,
+  useGetACategoryQuery,
+  useGetSubcategoriesQuery,
+  useGetColorsQuery,
+} = categoriesApi;
--- src/store/apis/productsApi.js
+++ src/store/apis/productsApi.js
@@ -1,46 +1,28 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const pause = async (duration) => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => resolve(), duration);
-	});
-};
-
 const productsApi = createApi({
-	reducerPath: "products",
-	tagTypes: ["Products"],
+  reducerPath: "products",
+  tagTypes: ["Products"],
 
-	baseQuery: fetchBaseQuery({
-		baseUrl: import.meta.env.VITE_REACT_APP_API_URL,
-		prepareHeaders: (headers) => {
-			// const token = getState().auth.token;
-			// console.log(token);
-			// If we have a token set in state, let's assume that we should be passing it.
-			headers.set(
-				"authorization",
-				`Bearer ${import.meta.env.VITE_REACT_APP_API_TOKEN}`
-			);
-			// if (!token) {
-			// }
-			return headers;
-		},
-		fetchFn: async (...args) => {
-			await pause(1000);
-			return fetch(...args);
-		},
-	}),
-	endpoints: (builder) => ({
-		getProducts: builder.query({
-			query: (type) => {
-				return `/products${type}`;
-			},
-			providesTags: () => [{ type: "Products", id: "LIST" }],
-		}),
-
-		getProductById: builder.query({
-			query: (productId) => `/products${productId}`,
-		}),
-	}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_REACT_APP_API_URL,
+    prepareHeaders: (headers) => {
+      headers.set(
+        "authorization",
+        `Bearer ${import.meta.env.VITE_REACT_APP_API_TOKEN}`
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: (type) => `/products${type}`,
+      providesTags: () => [{ type: "Products", id: "LIST" }],
+    }),
+    getProductById: builder.query({
+      query: (productId) => `/products${productId}`,
+    }),
+  }),
 });
 
 export { productsApi };
