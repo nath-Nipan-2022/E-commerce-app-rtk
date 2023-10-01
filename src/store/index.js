5ed194d6d221edb8e7923e4bcffa5f0d9e800c0c
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-01 23:46:30 +0530

    update for ordersApi

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,20 @@ import { productsApi } from "./apis/productsApi";
 import { categoriesApi } from "./apis/categoriesApi";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { CartsReducer, addCart, removeCart } from "./slices/cartsSlice";
+import { ordersApi } from "./apis/ordersApi";
 
-export const store = configureStore(
-  {
-    reducer: {
-      [productsApi.reducerPath]: productsApi.reducer,
-      [categoriesApi.reducerPath]: categoriesApi.reducer,
-      carts: CartsReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware()
-        .concat(productsApi.middleware)
-        .concat(categoriesApi.middleware),
+export const store = configureStore({
+  reducer: {
+    [productsApi.reducerPath]: productsApi.reducer,
+    [categoriesApi.reducerPath]: categoriesApi.reducer,
+    [ordersApi.reducerPath]: ordersApi.reducer,
+    carts: CartsReducer,
   },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(photosApi.middleware),
-);
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(productsApi.middleware)
+      .concat(categoriesApi.middleware)
+      .concat(ordersApi.middleware),
+});
 setupListeners(store.dispatch);
 export { addCart, removeCart };
