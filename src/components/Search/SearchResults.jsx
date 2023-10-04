c9be6fd2f5b6c8320ab181efc4b58137313ea21a
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-04 23:48:49 +0530

    state rename & styles change

--- src/components/Search/SearchResults.jsx
+++ src/components/Search/SearchResults.jsx
@@ -11,14 +11,18 @@ const initState = {
 };
 
 function SearchResults() {
-  const [state, setState] = useState(initState);
+  const [searchState, setSearchState] = useState(initState);
   const ref = useRef();
 
   useEffect(() => {
     const handler = (e) => {
       if (!ref.current || e.target.tagName === "A") return; // Link tag reloads page.
       if (!ref.current.contains(e.target)) {
-        setState((prev) => ({ ...prev, isOpen: false, selectedIndex: 0 }));
+        setSearchState((prev) => ({
+          ...prev,
+          isOpen: false,
+          selectedIndex: 0,
+        }));
       }
     };
     document.addEventListener("click", handler, true);
@@ -36,46 +40,39 @@ function SearchResults() {
 
   const filteredProducts = products?.data.filter((pdt) =>
     FILTER_ARR.some((key) =>
-      pdt.attributes[key].toLowerCase().includes(state.value.toLowerCase())
+      pdt.attributes[key]
+        .toLowerCase()
+        .includes(searchState.value.toLowerCase())
     )
   );
 
   const handleKeyBoardNavigation = (e) => {
+    let index = 0;
     if (e.key === "ArrowDown") {
-      let index = Math.min(
-        state.selectedIndex + 1,
+      index = Math.min(
+        searchState.selectedIndex + 1,
         filteredProducts.length - 1
       );
-      setState({ ...state, selectedIndex: index });
     } else if (e.key === "ArrowUp") {
-      let index = Math.max(state.selectedIndex - 1, 0);
-      setState({ ...state, selectedIndex: index });
+      index = Math.max(searchState.selectedIndex - 1, 0);
     }
+    setSearchState({ ...searchState, selectedIndex: index });
   };
 
   const navigate = useNavigate();
   const handleSubmit = () => {
-    navigate(`/products/${filteredProducts[state.selectedIndex].id}`);
-    setState({
-      ...state,
-      value: "",
-    });
+    navigate(`/products/${filteredProducts[searchState.selectedIndex].id}`);
+    setSearchState(initState);
   };
 
   const renderSearchResults = filteredProducts?.map((product, index) => {
-    const isActive = state.selectedIndex === index;
+    const isActive = searchState.selectedIndex === index;
 
     return (
       <li key={product.id} className="relative">
         <Link
           to={`/products/${product.id}`}
-          onClick={() =>
-            setState({
-              ...state,
-              value: product.attributes.name,
-              isOpen: false,
-            })
-          }
+          onClick={() => setSearchState(initState)}
           className={`py-1.5 px-5 font-medium block border-l-2 hover:bg-black/5 ${
             isActive ? "border-blue-500 bg-black/5" : "border-transparent"
           }`}
@@ -93,34 +90,36 @@ function SearchResults() {
   });
 
   const hasProducts =
-    state.value && state.isOpen && filteredProducts.length > 0;
+    searchState.value && searchState.isOpen && filteredProducts.length > 0;
 
   return (
     <div className="relative min-h-[44px] ml-auto md:ml-0">
       <div
-        className={`fixed z-10 top-0 py-3 md:py-0 left-0 w-full opacity-0 -translate-y-full transition bg-background-primary lg:bg-transparent md:static md:opacity-100 md:translate-y-0 ${
-          state.isOpen ? "opacity-100 translate-y-0" : ""
+        className={`fixed z-10 top-0 py-3 md:py-0 left-0 w-full opacity-0 transition bg-background-primary lg:bg-transparent md:static md:opacity-100 md:translate-x-0 ${
+          searchState.isOpen ? "opacity-100 translate-x-0" : "translate-x-full"
         }`}
       >
         {/* desktop search */}
         <SearchForm
-          value={state.value}
-          onChange={(value) => setState({ ...state, value })}
+          value={searchState.value}
+          onChange={(value) => setSearchState({ ...searchState, value })}
           onSubmit={handleSubmit}
           onKeyDown={handleKeyBoardNavigation}
-          onClick={() => setState({ ...state, isOpen: true })}
+          autoComplete="off"
+          onClick={() => setSearchState({ ...searchState, isOpen: true })}
           className={"hidden md:block"}
         />
 
         {/* mobile search */}
-        {state.isOpen && (
+        {searchState.isOpen && (
           <div className="flex justify-center md:hidden" ref={ref}>
             <SearchForm
               id="product-search_mobile"
               autoFocus={true}
-              value={state.value}
+              autoComplete="off"
+              value={searchState.value}
               onSubmit={handleSubmit}
-              onChange={(value) => setState({ ...state, value })}
+              onChange={(value) => setSearchState({ ...searchState, value })}
               onKeyDown={handleKeyBoardNavigation}
             />
           </div>
@@ -139,11 +138,11 @@ function SearchResults() {
       {/* search icon */}
       <span
         className={`search-icon-wrapper z-10 md:hidden ${
-          state.isOpen
+          searchState.isOpen
             ? "hidden"
             : "text-gray-600 hover:text-slate-900 cursor-pointer"
         }`}
-        onClick={() => setState({ ...state, isOpen: true })}
+        onClick={() => setSearchState({ ...searchState, isOpen: true })}
       >
         <FiSearch />
       </span>
