8843fc83c25d02365714730d129e80c5af9735b3
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-15 23:30:16 +0530

    remove unused assets

--- src/assets/categories/index.js
+++
@@ -1,8 +0,0 @@
-import headphone from "./headphone.png";
-import book from "./book.png";
-import handbag from "./handbag.png";
-import furniture from "./furniture-sofa.png";
-import laptop from "./laptop.png";
-import nike_shoe from "./nike shoe.png";
-
-export { headphone, handbag, laptop, nike_shoe, book, furniture };
