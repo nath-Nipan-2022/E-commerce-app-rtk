1cfd97f400fc04852664a99299256de80139262b
Nipan Debnath <debnathnipan719@gmail.com>
2023-10-18 01:19:43 +0530

    keep links constants in 1 file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,43 @@
-export { navLinks } from "./nav-links";
-export { footerLinks } from "./footer-links";
+// Nav links
+export const navLinks = [
+  {
+    id: 1,
+    label: "Categories",
+    hasDropdown: true,
+  },
+  {
+    id: 2,
+    label: "What's New",
+    path: "/whats-new",
+  },
+  {
+    id: 3,
+    label: "Wish List",
+    path: "/wishlist",
+  },
+  {
+    id: 4,
+    label: "Orders",
+    path: "/orders",
+  },
+];
+
+// Footer links
+export const footerLinks = [
+  {
+    title: "Category",
+    links: ["Women", "Men", "Shoes", "Accessories", "New Arrivals"],
+  },
+  {
+    title: "Links",
+    links: ["FAQ", "Pages", "Stores", "Compare", "Cookies"],
+  },
+  {
+    title: "About",
+    desc: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut et dolore. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt ut labore etdolore",
+  },
+  {
+    title: "Contact",
+    desc: "Lorem ipsum dolor sit amet conse cLorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sitamet conse ctetur adipisicing elit, seddo eiusmod tempor ",
+  },
+];
