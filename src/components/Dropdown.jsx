e092a50c9772c0cba018e0e670f1e6b1c41692ac
Nipan Debnath <debnathnipan719@gmail.com>
2023-12-06 23:59:34 +0530

    create reusable dropdown

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,19 @@
+function Dropdown({ isOpen, hasTitle, className, children, ...rest }) {
+  return (
+    <div
+      {...rest}
+      className={`absolute z-20 top-full left-0 ${
+        isOpen ? "block" : "hidden"
+      } ${className || ""}`}
+    >
+      <div className="bg-white lg:border lg:shadow-lg lg:rounded-lg animate-popUp">
+        {hasTitle && (
+          <h4 className="p-2 text-gray-600 border-b lg:p-4">{hasTitle}</h4>
+        )}
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
